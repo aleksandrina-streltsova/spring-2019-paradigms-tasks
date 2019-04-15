9828566e4440626bcc30f26b4e579ab1ac4370dc
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2019-04-15 19:18:27 +0300

    Get rid of one of counters

--- task06/printer.py
+++ task06/printer.py
@@ -1,72 +1,60 @@
 from model import *
+import textwrap
 
 
 class PrettyPrinter(ASTNodeVisitor):
+    INDENT = '    '
+
     def __init__(self):
-        self.number_of_indents = 0
         self.nesting_level = 0
         self.op_nesting_level = 0
 
     def visit_conditional(self, conditional):
-        result = self.number_of_indents * '    ' + 'if ('
+        result = 'if ('
         self.nesting_level += 1
         result += conditional.condition.accept(self)
         self.nesting_level -= 1
         result += ') {\n'
-
-        self.number_of_indents += 1
-        for expr in conditional.if_true:
-            result += expr.accept(self) + '\n'
-        self.number_of_indents -= 1
-        result += self.number_of_indents * '    ' + '}'
-
+        result += self.visit_expressions(conditional.if_true)
+        result += '}'
         if conditional.if_false:
             result += ' else {\n'
-            self.number_of_indents += 1
-            for expr in conditional.if_false:
-                result += expr.accept(self) + '\n'
-            self.number_of_indents -= 1
-            result += self.number_of_indents * '    ' + '}'
-
+            result += self.visit_expressions(conditional.if_false)
+            result += '}'
         return result
 
     def visit_function_definition(self, function_definition):
-        result = self.number_of_indents * '    ' + 'def '
+        result = 'def '
         result += function_definition.name + '('
         for arg in function_definition.function.args:
             result += arg + ', '
         if function_definition.function.args:
             result = result[:-2]
         result += ') {\n'
-
-        self.number_of_indents += 1
-        for statement in function_definition.function.body:
-            result += statement.accept(self) + '\n'
-        self.number_of_indents -= 1
-
+        result += self.visit_expressions(function_definition.function.body)
         result += '}'
         return result
 
     def visit_print(self, print):
-        result = self.number_of_indents * '    ' + 'print '
+        result = 'print '
         self.nesting_level += 1
         result += print.expr.accept(self) + ';'
         self.nesting_level -= 1
         return result
 
     def visit_read(self, read):
-        return self.number_of_indents * '    ' + 'read ' + read.name + ';'
+        return 'read ' + read.name + ';'
 
     def visit_number(self, number):
         result = str(number.value)
         if not self.nesting_level:
-            result = self.number_of_indents * '    ' + result + ';'
+            result += ';'
         return result
 
     def visit_reference(self, reference):
         result = str(reference.name)
         if not self.nesting_level:
-            result = self.number_of_indents * '    ' + result + ';'
+            result += ';'
         return result
 
     def visit_binary_operation(self, binary_op):
@@ -80,7 +68,7 @@ class PrettyPrinter(ASTNodeVisitor):
         if self.op_nesting_level:
             result = '(' + result + ')'
         if not (self.nesting_level or self.op_nesting_level):
-            result = self.number_of_indents * '    ' + result + ';'
+            result += ';'
         return result
 
     def visit_unary_operation(self, unary_op):
@@ -93,7 +81,7 @@ class PrettyPrinter(ASTNodeVisitor):
         if self.op_nesting_level:
             result = '(' + result + ')'
         if not (self.nesting_level or self.op_nesting_level):
-            result = self.number_of_indents * '    ' + result + ';'
+            result += ';'
         return result
 
     def visit_function_call(self, function_call):
@@ -107,11 +95,18 @@ class PrettyPrinter(ASTNodeVisitor):
         self.nesting_level -= 1
 
         if not self.nesting_level:
-            result = self.number_of_indents * '    ' + result + ';'
+            result += ';'
         return result
 
     def visit_function(self, function):
         pass
+
+    def visit_expressions(self, expressions):
+        result = ''
+        for expr in expressions or []:
+            result += expr.accept(self) + '\n'
+        return textwrap.indent(text=result, prefix=self.INDENT,
+                               predicate=lambda line: True)
 
 
 def pretty_print(program):
