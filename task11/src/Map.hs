cdf8458c40720e5ed39037bac01828c0c44e67a1
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2019-05-17 22:39:37 +0300

    solve task11

--- task11/src/Map.hs
+++ task11/src/Map.hs
@@ -1,7 +1,12 @@
 {-|
   Определение класса типов 'Map'.
 -}
+
+{-# LANGUAGE LambdaCase #-}
+
 module Map where
+
+import Data.Maybe
 
 {-|
   Поведение всех определённых здесь функций должно быть аналогично
@@ -31,45 +36,49 @@ class Map t where
     singleton :: k -> a -> t k a
 
     fromList :: Ord k => [(k, a)] -> t k a
-    fromList = undefined {- insert -}
+    fromList = foldl (\t (k, v) -> insert k v t) empty
 
     toAscList :: t k a -> [(k, a)]
 
     insert :: Ord k => k -> a -> t k a -> t k a
-    insert = undefined {- insertWith -}
+    insert = insertWith const
 
     insertWith :: Ord k => (a -> a -> a) -> k -> a -> t k a -> t k a
-    insertWith = undefined {- alter -}
+    insertWith f k v = alter (\case
+            Nothing -> Just v
+            Just x -> Just (f v x)) k
 
     insertWithKey :: Ord k => (k -> a -> a -> a) -> k -> a -> t k a -> t k a
-    insertWithKey = undefined {- insertWith -}
+    insertWithKey f k = insertWith (f k) k
 
     delete :: Ord k => k -> t k a -> t k a
-    delete = undefined {- alter -}
+    delete = alter (const Nothing)
 
     adjust :: Ord k => (a -> a) -> k -> t k a -> t k a
-    adjust = undefined {- alter -}
+    adjust f = alter (fmap f)
 
     adjustWithKey :: Ord k => (k -> a -> a) -> k -> t k a -> t k a
-    adjustWithKey = undefined {- adjust -}
+    adjustWithKey f k = adjust (f k) k
 
     update :: Ord k => (a -> Maybe a) -> k -> t k a -> t k a
-    update = undefined {- alter -}
+    update f = alter (\case
+            Nothing -> Nothing
+            Just x -> f x)
 
     updateWithKey :: Ord k => (k -> a -> Maybe a) -> k -> t k a -> t k a
-    updateWithKey = undefined {- update -}
+    updateWithKey f k = update (f k) k
 
     alter :: Ord k => (Maybe a -> Maybe a) -> k -> t k a -> t k a
 
     lookup :: Ord k => k -> t k a -> Maybe a
 
     member :: Ord k => k -> t k a -> Bool
-    member = undefined {- lookup -}
+    member k t = isJust (Map.lookup k t)
 
     notMember :: Ord k => k -> t k a -> Bool
-    notMember = undefined {- member -}
+    notMember k t = not (member k t)
 
     null :: t k a -> Bool
-    null = undefined {- size -}
+    null t = (size t == 0)
 
     size :: t k a -> Int
--- task11/test/TestMap.hs
+++ task11/test/TestMap.hs
@@ -27,10 +27,114 @@ mapTests :: Map m => String -> Proxy m -> TestTree
 mapTests name (_ :: Proxy m) =
     -- Чтобы можно было связать типовую переменную m здесь и в let ниже, нужно расширение ScopedTypeVariables.
     testGroup name [
-        testGroup "Smoke tests" [
+        testGroup "Test toAscList, fromList" [
             testCase "toAscList . fromList sorts list" $
                 let tr = fromList [(2, "a"), (1, "b"), (3, "c"), (1, "x")] :: m Int String in
                 toAscList tr @?= [(1, "x"), (2, "a"), (3, "c")]
+        ],
+        testGroup "Test insert" [
+            testCase "insert in empty map" $
+                let map = insert 5 "x" (empty :: m Int String) in
+                Map.lookup 5 map @?= Just "x"
+            ,
+            testCase "insert new key" $
+                let map = insert 5 "x" (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                Map.lookup 5 map @?= Just "x"
+            ,
+            testCase "insert existent key" $
+                let map = insert 1 "x" (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                Map.lookup 1 map @?= Just "x"
+        ],
+        testGroup "Test insertWith" [
+            testCase "insertWith in empty map" $
+                let map = insertWith (++) 5 "x" (empty :: m Int String) in
+                Map.lookup 5 map @?= Just "x"
+            ,
+            testCase "insertWith new key" $
+                let map = insertWith (++) 5 "x" (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                Map.lookup 5 map @?= Just "x"
+            ,
+            testCase "insertWith existent key" $
+                let map = insertWith (++) 1 "x" (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                Map.lookup 1 map @?= Just "xb"
+        ],
+        testGroup "Test insertWithKey" [
+            testCase "insertWithKey in empty map" $
+                let f k new_v old_v = show k ++ new_v ++ old_v in
+                let map = insertWithKey f 5 "x" (empty :: m Int String) in
+                Map.lookup 5 map @?= Just "x"
+            ,
+            testCase "insertWithKey existent key" $
+                let f k new_v old_v = show k ++ new_v ++ old_v in
+                let map = insertWithKey f 1 "x" (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                Map.lookup 1 map @?= Just "1xb"
+        ],
+        testGroup "Test delete" [
+            testCase "delete nonexistent key" $
+                let map = delete 5 (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                toAscList map @?= [(1, "b"), (2, "a"), (3, "c")]
+            ,
+            testCase "delete existent key" $
+                let map = delete 1 (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                toAscList map @?= [(2, "a"), (3, "c")]
+        ],
+        testGroup "Test adjust" [
+            testCase "adjust nonexistent key" $
+                let map = adjust ("new " ++) 5 (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                Map.lookup 5 map @?= Nothing
+            ,
+            testCase "delete existent key" $
+                let map = adjust ("new " ++) 1 (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                Map.lookup 1 map @?= Just "new b"
+        ],
+        testGroup "Test adjustWithKey" [
+            testCase "adjustWithKey nonexistent key" $
+                let f k v = show k ++ v in
+                let map = adjustWithKey f 5 (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                Map.lookup 5 map @?= Nothing
+                ,
+            testCase "adjustWithKey existent key" $
+                let f k v = show k ++ v in
+                let map = adjustWithKey f 1 (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                Map.lookup 1 map @?= Just "1b"
+        ],
+        testGroup "Test update" [
+            testCase "key is deleted by update" $
+                let f x = if x == "a" then Just "new a" else Nothing in
+                let map = update f 1 (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                Map.lookup 1 map @?= Nothing
+            ,
+            testCase "key is updated by update" $
+                let f x = if x == "a" then Just "new a" else Nothing in
+                let map = update f 2 (fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String) in
+                Map.lookup 2 map @?= Just "new a"
+        ],
+        testGroup "Test memeber" [
+            testCase "key is in map" $
+                let map = fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String in
+                member 1 map @?= True
+            ,
+            testCase "key isn't in map" $
+                let map = fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String in
+                member 5 map @?= False
+        ],
+        testGroup "Test notMemeber" [
+            testCase "key is in map" $
+                let map = fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String in
+                notMember 1 map @?= False
+            ,
+            testCase "key isn't in map" $
+                let map = fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String in
+                notMember 5 map @?= True
+        ],
+        testGroup "Test null" [
+            testCase "empty map" $
+                let map = empty :: m Int String in
+                Map.null map @?= True
+            ,
+            testCase "non empty map" $
+                let map = fromList [(2, "a"), (1, "b"), (3, "c")] :: m Int String in
+                Map.null map @?= False
         ]
     ]
 
